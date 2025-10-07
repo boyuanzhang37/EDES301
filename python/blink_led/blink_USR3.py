2d0407698e07108315c9c06192d38c94fa4bdc9f
Demo User <debian@beaglebone.localdomain>
2025-10-07 21:45:18 +0000

    assignment 4

---
+++ python/blink_led/blink_USR3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PocketBeagle LED Blink Script
+=============================
+
+License:   
+Copyright 2025 - Boyuan Zhang
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+This script blinks the 'usr3' LED on the PocketBeagle board at a configurable rate.
+It uses the sysfs interface to control the LED by writing to the brightness file.
+
+Usage:
+    Run with Python 3 and appropriate permissions:
+        sudo python3 blink_usr3.py
+
+    To blink at 5 Hz (5 times per second), the script uses a 0.1s cycle (0.05s on, 0.05s off).
+
+Notes:
+    - Requires access to /sys/class/leds/beaglebone:green:usr3/brightness
+    - May need root privileges depending on system configuration
+"""
+
+import os
+import time
+
+LED_PATH = "/sys/class/leds/beaglebone:green:usr3/brightness"
+
+def blink_led(blinks_per_second=5):
+    interval = 1 / (blinks_per_second * 2)  # Half-period for on/off
+    while True:
+        with open(LED_PATH, 'w') as led:
+            led.write('1')
+        time.sleep(interval)
+
+        with open(LED_PATH, 'w') as led:
+            led.write('0')
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    try:
+        blink_led()
+    except KeyboardInterrupt:
+        print("\nBlinking stopped.")
+
